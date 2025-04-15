4cf23201e4defdee7bdc1bf6cc020efbac076ab3
Velo <105860237+velo-app[bot]@users.noreply.github.com>
2025-04-15 00:03:38 +0000

    Initial commit

---
+++ src/pages/masterPage.js
@@ -0,0 +1,22 @@
+
+
+
+/**
+*	Adds an event handler that runs when an element is no longer
+ displayed in the viewable part of the current window.
+*	 @param {$w.Event} event
+*/
+export function languageSelector32_viewportLeave(event) {
+	// This function was added from the Properties & Events panel. To learn more, visit http://wix.to/UcBnC-4
+	// Add your code for this event here: 
+}
+
+/**
+*	Adds an event handler that runs when an element is displayed
+ in the viewable part of the current window.
+*	 @param {$w.Event} event
+*/
+export function languageSelector32_viewportEnter(event) {
+	// This function was added from the Properties & Events panel. To learn more, visit http://wix.to/UcBnC-4
+	// Add your code for this event here: 
+}
